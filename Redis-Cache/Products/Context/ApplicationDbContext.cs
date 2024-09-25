c745ffd4c3b4d37ee96680b46bf6bf948548e86e
Dinesh Vasudevan <dineshvsvce@gmail.com>
2024-09-25 09:00:21 +0530

    Add project files.

---
+++ Redis-Cache/Products/Context/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Products.Model;
+
+namespace Products.Context;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+    public DbSet<ConsumerProduct> Products { get; set; }
+}
---
+++ Redis-Cache/Products/Controllers/ConsumerProductsController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Distributed;
+using Products.Context;
+using System.Text;
+using System.Text.Json;
+
+namespace Products.Model.Product.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConsumerProductsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IDistributedCache _cache;
+
+        public ConsumerProductsController(ApplicationDbContext context, IDistributedCache cache)
+        {
+            _context = context;
+            _cache = cache;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var cacheKey = "GET_ALL_PRODUCTS";
+            List<ConsumerProduct> consumerProducts;
+
+            // Get data from cache
+            var cachedData = await _cache.GetAsync(cacheKey);
+            if (cachedData != null)
+            {
+                // If data found in cache, encode and deserialize cached data
+                var cachedDataString = Encoding.UTF8.GetString(cachedData);
+                consumerProducts = JsonSerializer.Deserialize<List<ConsumerProduct>>(cachedDataString) ?? new List<ConsumerProduct>();
+            }
+            else
+            {
+                // If not found, then fetch data from database
+                consumerProducts = await _context.Products.ToListAsync();
+
+                // serialize data
+                var cachedDataString = JsonSerializer.Serialize(consumerProducts);
+                var newDataToCache = Encoding.UTF8.GetBytes(cachedDataString);
+
+                // set cache options 
+                var options = new DistributedCacheEntryOptions()
+                    .SetAbsoluteExpiration(DateTime.Now.AddMinutes(2))
+                    .SetSlidingExpiration(TimeSpan.FromMinutes(1));
+
+                // Add data in cache
+                await _cache.SetAsync(cacheKey, newDataToCache, options);
+            }
+
+            return Ok(consumerProducts);
+        }
+    }
+}
